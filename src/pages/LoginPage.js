8d801e64551325e5fa497375b7b9745a5e7e0554
Andrew Martins <andrew_omartins@yahoo.com>
2022-08-23 05:18:28 -0700

    added jwt configuration to login file

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+export const handleSubmit = (email, pass) => {
+    //sample user regristration using reqres
+    const loginPayload = {
+        email: 'eve.holy@reqres.in',
+        password: 'cityslicka'
+    }
+
+    axios.post("https://reqres.in/api/login", loginPayload)
+        .then(response => {
+            //get token from response
+            const tokken = response.data.tokken
+
+             //set JWT token to local 
+             localStorage.setItem('token', token)
+
+             //set token to axios common header
+             setAuthToken(token)
+ 
+             //redirect user to home page
+             window.location.href = '/'
+        })
+        .catch(err => console.log)
+}
